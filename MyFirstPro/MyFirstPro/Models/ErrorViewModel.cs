7365f4f5c4719c4bcba38437d4728ccf71a1d7a4
hzw <2583872922@qq.com>
2020-06-07 11:26:38 +0800

    commit

---
+++ MyFirstPro/MyFirstPro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyFirstPro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
