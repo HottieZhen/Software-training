d533082852f1e68fc8f22a1f97b9e20f9da8240c
hzw <2583872922@qq.com>
2020-06-14 13:25:50 +0800

    Assignment6

---
+++ TinyShop/Data/ProductDO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data
+{
+    [Table("Products")]
+    public class ProductDO
+    {
+        [Key] public long Id { get; set; }
+        [StringLength(100)] public string ProductNumber { get; set; }
+        [StringLength(100)] public string ProductName { get; set; }
+        [StringLength(100)] public string ProductType { get; set; }
+        [StringLength(100)] public double Price { get; set; }
+    }
+}
