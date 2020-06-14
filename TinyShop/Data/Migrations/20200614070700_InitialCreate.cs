9c128c2c854308fe0784ec866403fbdd868e8e6c
hzw <2583872922@qq.com>
2020-06-14 15:14:49 +0800

    modify

---
+++ TinyShop/Data/Migrations/20200614070700_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ProductNumber = table.Column<string>(maxLength: 100, nullable: true),
+                    ProductName = table.Column<string>(maxLength: 100, nullable: true),
+                    ProductType = table.Column<string>(maxLength: 100, nullable: true),
+                    Price = table.Column<double>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ TinyShop/Data/a.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Data
+{
+    public class a : IDesignTimeDbContextFactory<DataContext>
+    {
+        public a()
+        {
+        }
+
+        public DataContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+            optionsBuilder.UseMySql("server=localhost;user id=root;password=Whz123456,;database=tinyshop;charset=utf8;sslMode=None");
+            return new DataContext(optionsBuilder.Options);
+        }
+    }
+}
